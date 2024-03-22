4782a19b13d02ddcb1e3e44ee21f4d429c867a05
Steven <stevengf14@gmail.com>
2024-03-21 23:28:05 -0500

    Working with component styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  template: `<app-server></app-server> <app-server></app-server>`,
   styleUrl: './servers.component.css',
 })
 export class ServersComponent {}
